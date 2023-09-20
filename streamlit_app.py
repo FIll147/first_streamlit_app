f363a11db4e142d94e562f2375100f847b274e0e
Fill147 <145622506+Fill147@users.noreply.github.com>
2023-09-20 20:51:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('BreakFast Menu')
+streamlit.text('Omega3 & Blueberry')
+streamlit.text('Kale, Spinach & ROcket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
